fbff06929866e4e889de080c1ee8378bece97c8d
Kaushang Surya <141481179+kaushang@users.noreply.github.com>
2025-01-29 23:26:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const postModel = require('./models/postModel');
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
