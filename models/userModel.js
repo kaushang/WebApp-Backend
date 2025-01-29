4e65f7d533a2c520b778eb89635597b5cc6a497d
Kaushang Surya <141481179+kaushang@users.noreply.github.com>
2025-01-29 23:44:23 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/users');
+// mongoose.connect('mongodb://localhost:27017/users');
 
 const userSchema = mongoose.Schema({
     email: String,
