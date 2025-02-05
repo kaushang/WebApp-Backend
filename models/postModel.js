1eb025d2eb258e2048be030c724737a552b38a76
Kaushang Surya <kaushangsurya29@gmail.com>
2025-02-05 17:30:00 +0530

    corrections

--- models/postModel.js
+++ models/postModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
@@ -12,21 +11,4 @@ const postSchema = mongoose.Schema({
     },
     content: String,
 });
-
-=======
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user"
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    content: String,
-});
-
->>>>>>> 83fa5d85edb70ac2fd9b912e96b66898c974a188
 module.exports = mongoose.model("post", postSchema);
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/users');
 
@@ -16,22 +15,3 @@ const userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("user", userSchema);
-=======
-const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/users');
-
-const userSchema = mongoose.Schema({
-    email: String,
-    username: String,
-    name: String,
-    password: String,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "user"
-        }
-    ]
-});
-
-module.exports = mongoose.model("user", userSchema);
->>>>>>> 83fa5d85edb70ac2fd9b912e96b66898c974a188
