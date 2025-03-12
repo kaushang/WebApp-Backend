dd7ad816d748e4932c278f7bc12379217da120a9
Kaushang Surya <kaushangsurya29@gmail.com>
2025-03-13 00:40:33 +0530

    docker file added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useUnifiedTopology: true
 })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
